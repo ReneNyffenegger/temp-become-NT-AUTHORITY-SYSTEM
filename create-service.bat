427febe874da1757a8b43343cae75dae7e6000b6
Rene Nyffenegger <mail@renenyffenegger.ch>
2020-06-03 07:58:53 +0200

    Update create-service.bat

--- create-service.bat
+++ create-service.bat
@@ -10,3 +10,10 @@ rem
 rem    [SC] CreateService SUCCESS
 rem
 rem    WARNING:  The service becomeSystem is configured as interactive whose support is being deprecated. The service may not function properly.
+
+
+rem
+rem should the value of NoInteractiveServices be set to 0 (or 1) under
+rem    HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Windows
+rem ?
+rem
